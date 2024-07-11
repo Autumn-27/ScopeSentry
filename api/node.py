f18132058ce6bb3cf97cf4cec4456cfea49e36e6
Autumn.home <autumn@home.com>
2024-07-11 19:16:53 +0800

    fix 扫描任务忽略节点时间差

--- api/node.py
+++ api/node.py
@@ -138,17 +138,17 @@ async def get_redis_online_data(redis_con):
             name = key.split(":")[1]
             hash_data = await redis.hgetall(key)
             if hash_data.get('state') == '1':
-                update_time_str = hash_data.get('updateTime')
-                if update_time_str:
-                    update_time = datetime.strptime(update_time_str, '%Y-%m-%d %H:%M:%S')
-                    time_difference = (
-                                datetime.strptime(get_now_time(), "%Y-%m-%d %H:%M:%S") - update_time).total_seconds()
-                    logger.info(f'节点时间差：{time_difference}, {get_now_time()}, {update_time}')
-                    if time_difference > NODE_TIMEOUT:
-                        await asyncio.create_task(update_redis_data(redis, key))
-                        hash_data['state'] = '3'
-                    else:
-                        result.append(name)
+                # update_time_str = hash_data.get('updateTime')
+                # if update_time_str:
+                #     update_time = datetime.strptime(update_time_str, '%Y-%m-%d %H:%M:%S')
+                #     time_difference = (
+                #                 datetime.strptime(get_now_time(), "%Y-%m-%d %H:%M:%S") - update_time).total_seconds()
+                #     logger.info(f'节点时间差：{time_difference}, {get_now_time()}, {update_time}')
+                #     if time_difference > NODE_TIMEOUT:
+                #         await asyncio.create_task(update_redis_data(redis, key))
+                #         hash_data['state'] = '3'
+                #     else:
+                result.append(name)
         return result
 
 
