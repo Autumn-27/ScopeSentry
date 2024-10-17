4645f6ab931281339cb6dbc6731a54d039bdac0c
Autumn.home <autumn@home.com>
2024-10-17 22:42:03 +0800

    整合字典管理

--- api/dictionary/__init__.py
+++ api/dictionary/__init__.py
@@ -8,7 +8,9 @@ from fastapi import APIRouter
 
 from .dir import router as dir_router
 from .subdomain import router as subdoamin_router
+from .manage import router as manage_routeer
 router = APIRouter()
 
 router.include_router(dir_router, prefix="/dir")
 router.include_router(subdoamin_router, prefix="/subdomain")
+router.include_router(manage_routeer, prefix="/manage")
